94f3f45a9ed0ee31910177dfa18fffa0fbada168
Marcel Klehr <mklehr@gmx.net>
2015-10-21 14:43:08 +0200

    Rename to hive-plugin-cursor-broadcast-ckeditor

--- client.js
+++ client.js
@@ -33,7 +33,7 @@ function setup(plugin, imports, register) {
 
   var link = document.createElement('link')
   link.setAttribute('rel', 'stylesheet')
-  link.setAttribute('href', ui.baseURL+'/static/hive-plugin-ckeditor-cursor-broadcast/css/index.css')
+  link.setAttribute('href', ui.baseURL+'/static/hive-plugin-cursor-broadcast-ckeditor/css/index.css')
   document.head.appendChild(link)
 
   ui.page('/documents/:id', function(ctx, next) {
